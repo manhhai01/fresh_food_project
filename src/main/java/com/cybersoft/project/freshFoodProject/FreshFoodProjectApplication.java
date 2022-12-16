6f82a6fa9b0f6156ec9b26bc0960a2cf1830354e
Hai La <Lahai7744@gmail.com>
2022-12-16 19:58:57 +0700

    first commit project

---
+++ src/main/java/com/cybersoft/project/freshFoodProject/FreshFoodProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cybersoft.project.freshFoodProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshFoodProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshFoodProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersoft/project/freshFoodProject/FreshFoodProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.project.freshFoodProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshFoodProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
